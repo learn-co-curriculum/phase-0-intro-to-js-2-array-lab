42f47e514866c0c50461a094c76498ca9104d2a3
Kellye Greene <kellyecodes@gmail.com>
2019-05-20 12:26:05 -0400

    Removes weird test that breaks everything

--- test/indexTest.js
+++ test/indexTest.js
@@ -3,10 +3,6 @@ describe('index.js', function () {
     it('is assigned an initial value of ["Milo", "Otis", "Garfield"]', function () {
       expect(drivers).to.have.ordered.members(["Milo", "Otis", "Garfield"]);
     });
-
-    it('is declared with const', function () {
-      expect(function () { drivers = []; }).to.throw(TypeError);
-    });
   });
 
   describe('Array functions', function () {
