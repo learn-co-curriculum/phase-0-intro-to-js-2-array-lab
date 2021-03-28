1152a059f3b2f0cb0ac7e9a8fbb319d95f2ce0cc
raymond zhao <raymondzhao158@gmail.com>
2021-03-28 04:04:39 -0700

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Write your solution here!
+var cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name) {
+    return cats.push(name)
+}
+
+function destructivelyPrependCat(name) {
+    return cats.unshift(name)
+}
+
+function destructivelyRemoveLastCat(name) {
+    return cats.pop(name)
+}
+
+function destructivelyRemoveFirstCat(name) {
+    return cats.shift(name)
+}
+
+function appendCat(name) {
+    return [...cats, name]
+}
+
+function prependCat(name) {
+    return [name, ...cats]
+}
+
+function removeLastCat() {
+    return cats.slice(0,-1)
+}
+
+function removeFirstCat() {
+    return cats.slice(1)
+}
