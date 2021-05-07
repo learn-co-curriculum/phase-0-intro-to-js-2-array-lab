c1944c526e12bebacb8258149f31808d51c9b154
ihollander <ianholla@gmail.com>
2021-05-07 14:18:22 -0400

    rm file-system dependency

--- test/helpers.js
+++ test/helpers.js
@@ -1,21 +1,26 @@
-const chai = require('chai')
-const sinon = require('sinon')
-global.expect = chai.expect
-const fs = require('file-system')
-const jsdom = require('mocha-jsdom')
-const path = require('path')
-const babel = require('babel-core');
+const chai = require("chai");
+const sinon = require("sinon");
+global.expect = chai.expect;
+const fs = require("fs");
+const jsdom = require("mocha-jsdom");
+const path = require("path");
+const babel = require("babel-core");
 
-const html = fs.readFileSync(path.resolve(__dirname, '..', 'index.html'), 'utf-8')
+const html = fs.readFileSync(
+  path.resolve(__dirname, "..", "index.html"),
+  "utf-8"
+);
 
 const babelResult = babel.transformFileSync(
-  path.resolve(__dirname, '..', 'index.js'), {
-    presets: ['env']
+  path.resolve(__dirname, "..", "index.js"),
+  {
+    presets: ["env"],
   }
 );
 
-const src = babelResult.code
+const src = babelResult.code;
 
 jsdom({
-  html, src
+  html,
+  src,
 });
