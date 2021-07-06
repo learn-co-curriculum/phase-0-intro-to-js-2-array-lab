580cb8c5d8729ceb0a920583f9623b0a4225ada0
Ajmol Uddin <uddin.ajmol@gmail.com>
2021-07-06 03:00:41 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 // Write your solution here!
+let cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name) {
+  cats.push(name);
+}
+
+function destructivelyPrependCat(name) {
+  cats.unshift(name);
+}
+
+function destructivelyRemoveLastCat() {
+  cats.pop();
+}
+
+function destructivelyRemoveFirstCat() {
+  cats.shift();
+}
+
+function appendCat(name) {
+  let newCats = [...cats];
+  newCats.push(name);
+  return newCats;
+}
+
+function prependCat(name) {
+  let newCats = [...cats];
+  newCats.unshift(name);
+  return newCats;
+}
+
+function removeLastCat() {
+  let newCats = [...cats];
+  newCats.pop();
+  return newCats;
+}
+
+function removeFirstCat() {
+  let newCats = [...cats];
+  newCats.shift();
+  return newCats;
+}
