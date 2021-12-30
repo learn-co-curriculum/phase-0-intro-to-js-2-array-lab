61326f9bed0fd5208f2160f2fc66d7d7f5a8ac47
Themanual22 <djamosry22@gmail.com>
2021-12-30 12:31:31 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-// Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name) {
+  cats.push(name);
+}
+function destructivelyPrependCat(name) {
+  cats.unshift(name);
+}
+function destructivelyRemoveLastCat(name) {
+  cats.pop(name);
+}
+function destructivelyRemoveFirstCat(name) {
+  cats.shift(name)  
+} 
+const newcats = ["Broom"]
+function appendCat(name){
+  let newcats = [...cats, "Broom"]
+  return newcats
+}
+const orangecats = ["Arnold"];
+function prependCat (name) {
+  let ornagecats =["Arnold", ...cats];
+  return ornagecats
+}
+const whitecats = (["Milo", "Otis"]) 
+function removeLastCat (name) {
+  whitecats.slice(name);
+  return whitecats
+}
+const blackcats = ["Otis","Garfield"] 
+function removeFirstCat (name) {
+  blackcats.slice(name);
+  return blackcats
+}
