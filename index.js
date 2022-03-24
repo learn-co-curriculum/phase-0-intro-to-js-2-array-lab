50c25a9441e93e77c1123ff102813df7451b7012
Byron <byrondonohue@gmail.com>
2022-03-24 18:39:35 -0400

    commit tree 62029c7557dbb2707d0b2f223c0029d4d7c0a0c2

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
-// Write your solution here!
+const cats = [
+    "Milo", "Otis", "Garfield"
+]
+
+function  destructivelyAppendCat(name) { 
+cats.push(name)
+}
+
+function destructivelyPrependCat(name) {
+    cats.unshift(name)
+}
+
+function  destructivelyRemoveLastCat(name) {
+    cats.pop()
+}
+
+function destructivelyRemoveFirstCat(name) {
+    cats.shift()
+}
+
+function  appendCat(name) {
+    const copyCat = [...cats, name]
+    return copyCat
+}
+
+function prependCat(name) {
+    const copyCat = [name, ...cats]
+    return copyCat
+}
+function removeLastCat(name) {
+    const copyCats = [...cats,]
+    copyCats.pop()
+    return copyCats
+}
+function removeFirstCat(name) {
+    const copyCat = [...cats]
+    copyCat.shift()
+    return copyCat
+}// Write your solution here!
