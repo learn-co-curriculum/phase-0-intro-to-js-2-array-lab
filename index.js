8a498594378b1cbafbc78099a2efa43350e45fee
Jordan Jennings <jordanjennings12@gmail.com>
2022-06-22 18:16:29 -0400

    final commit

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Write your solution here!
+let cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name) {
+    cats.push(name);
+}
+
+function destructivelyPrependCat(name) {
+    cats.unshift(name);
+}
+
+function destructivelyRemoveLastCat(){
+    cats.pop();
+}
+
+function destructivelyRemoveFirstCat(){
+    cats.shift();
+}
+
+function appendCat(name){
+    let newArray = [...cats.push(name)];
+    return newArray;
+}
