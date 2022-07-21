9f53c4513a3f49ad20136b353e60998dd7e27939
rob <robertmaticke12@gmail.com>
2022-07-21 13:05:48 -0400

    commit tree 6df039929d66d89b43b7d4b3fa8957efc92a6285

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat() {
+    cats.push("Ralph");
+};
+
+function destructivelyPrependCat() {
+    cats.unshift("Bob");
+};
+
+function destructivelyRemoveLastCat() {
+    cats.splice(-1);
+};
+
+function destructivelyRemoveFirstCat() {
+    cats.splice(0, 1);
+};
+
+function appendCat() {
+    const copyOfCats = [...cats, "Broom"];
+
+    return copyOfCats;
+}
+
+function prependCat() {
+    const copyOfCats2 = ["Arnold", ...cats];
+
+    return copyOfCats2;
+}
+
+function removeLastCat() {
+    let copyOfCats3 = cats.slice(0, cats.length - 1);
+
+    return copyOfCats3
+}
+
+function removeFirstCat() {
+    let copyOfCats4 = cats.slice(1)
+
+    return copyOfCats4
+}
