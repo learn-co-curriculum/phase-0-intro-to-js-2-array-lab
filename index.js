06f6c506eda4cc56b6a42ae220b720d9fa63b34e
Chen <chen.jiang.1206@gmail.com>
2023-03-09 17:49:01 -0500

    commit tree 650d09f22d2607132ac1793641cb71003a31cbe4

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendCat(name) {
+    return cats.push(name);
+}
+
+function destructivelyPrependCat(name) {
+    return cats.unshift(name);
+}
+
+function destructivelyRemoveLastCat() {
+    return cats.pop();
+}
+
+function destructivelyRemoveFirstCat() {
+    return cats.shift();
+}
+
+function appendCat(name) {
+    const newCats = [...cats, name];
+    return newCats;
+}
+
+function prependCat(name) {
+    const newCats = [name, ...cats];
+    return newCats;
+}
+
+function removeLastCat() {
+    const newCats = cats.slice(0, -1);
+    return newCats;
+
+}
+
+function removeFirstCat() {
+    const newCats = cats.slice(1);
+    return newCats;
+}
